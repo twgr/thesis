8db876f2125f6685486ab59dae4abd0151f0b64d
twgr <twgr@robots.ox.ac.uk>
2017-09-23 20:12:22 +0100

    cleaning earlier part of inf section

--- code/inf_example.m
+++ code/inf_example.m
@@ -17,9 +17,9 @@ post = @(t,y) prior(t).*lik(t,y)/I;
 posts = post(thetas,y);
 
 line_width = 5;
-font_size = 70;
+font_size = 90;
 axlim = [0,15];
-aylim = [0,0.5];
+aylim = [0,0.42];
 interpreter = 'latex';
 
 figure('units','normalized','outerposition',[0 0 1 1]);
@@ -27,10 +27,10 @@ plot(thetas,pris,'LineWidth',line_width);
 hold on
 plot(thetas,liks,'LineWidth',line_width);
 plot(thetas,posts,'LineWidth',line_width);
-plot(thetas,0.5*cumsum(posts)*(thetas(2)-thetas(1)),'LineWidth',line_width);
+plot(thetas,0.4*cumsum(posts)*(thetas(2)-thetas(1)),'LineWidth',line_width);
 xlabel('$\theta$','Interpreter',interpreter);
-%ylabel('Probability','Interpreter',interpreter);
-legend({'$p(\theta)$','$p(y=5|\theta)$','$p(\theta|y=5)$','$\frac{1}{2} P(\Theta<\theta|y=5)$'},'Interpreter',interpreter);
+ylabel('Probability Density','Interpreter',interpreter);
+legend({'$p(\theta)$','$p(y=5|\theta)$','$p(\theta|y=5)$','$0.4 P(\Theta<\theta|y=5)$'},'Interpreter',interpreter);
 xlim(axlim);
 ylim(aylim);
 set(gca,'FontSize',font_size);
